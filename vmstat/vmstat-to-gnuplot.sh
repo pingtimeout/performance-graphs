c89bdb3861f842bd3f5e89f86d22b5d54e4785e9
Pierre Laporte <pierre@pingtimeout.fr>
2015-01-20 11:41:38 +0100

    Licence to GPL

--- vmstat/vmstat-to-gnuplot.sh
+++ vmstat/vmstat-to-gnuplot.sh
@@ -1,6 +1,22 @@
 #!/bin/sh
 
 #
+# Copyright (c) 2014, Pierre Laporte
+#
+# This code is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 3 only, as
+# published by the Free Software Foundation.
+#
+# This code is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this work; if not, see <http://www.gnu.org/licenses/>.
+#
+
+#
 # This scripts generates graphs from a vmstat output.
 #
 # Usage : ./vmstat-to-gnuplot.sh vmstat-example
